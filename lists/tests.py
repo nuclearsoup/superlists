840cf407f4b79a9a7f59e210896e9fb6a4bb5bd7
nuclearsoup <kevinpercy@gmail.com>
2014-04-14 17:35:32 +0100

    First unit test and url config and dummy home

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,13 @@
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from lists.views import home_page
 
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
 class SmokeTest(TestCase):
 
