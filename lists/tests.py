87e83740674eb7b058d34e11ef9f5255f7ba689b
nuclearsoup <kevinpercy@gmail.com>
2014-04-14 18:02:15 +0100

    we have a web page

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 
 
@@ -9,7 +11,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-class SmokeTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
 
-    def test_bad_maths(self):
-        self.assertEqual(1+1, 3)
