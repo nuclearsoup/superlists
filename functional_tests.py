157ec1e420f196718a06fe8280e7fa0679a2e5ea
nuclearsoup <kevinpercy@gmail.com>
2014-04-16 10:54:29 +0100

    the calm before cessation of cheating

--- functional_tests.py
+++ functional_tests.py
@@ -16,27 +16,47 @@ class NewVisitorTest(unittest.TestCase):
         '''
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do', self.browser.title)
+
         header_text = self.browser.find_element_by_tag_name('h1').text
+
         self.assertIn('To-Do', header_text)
+
         # invited to add todo straight away
+
         inputbox = self.browser.find_element_by_id('id_new_item')
+
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
             )
+
         # types buy peacock feathers into text to
-        inputbox.send_keys('Buy peacock feathers')
         # when hits enter the page updates and the new page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
 
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        
+
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New To-Do item did not appear in table"
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # Being methodical she enters a second item:
+        # Use feathers to make fly
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use feathers to make fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use feathers to make fly',
+            [row.text for row in rows]
             )
+
         self.fail('Finish the test!')
 
 
