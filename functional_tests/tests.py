5b7fb93c86e3754900f6abc25a43ad7e1147e700
nuclearsoup <kevinpercy@gmail.com>
2014-04-21 09:59:06 +0100

    first steps of ft for styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -97,5 +97,26 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, she goes back to sleep
 
+    def test_layout_and_styling(self):
+        # Edith goes to the homepage
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # She notices the inbox is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+            )
+        # She starts a new list and notices the input is nicely cenred ther too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+            )
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
+
+
