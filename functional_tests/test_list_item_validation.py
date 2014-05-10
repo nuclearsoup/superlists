89d49ea301ecb2866e5d5c8af5c153f56cf6b457
nuclearsoup <kevinpercy@gmail.com>
2014-05-10 15:40:06 +0100

    Empty list test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,17 +4,25 @@ from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
 
-    @skip
     def test_cannot_add_empty_list_items(self):
         # Mistakenly enters blank object
-
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # Page refreshes telling her entry cannot be empty
-
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
         # Try again with test for item which now succeeds
-
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+        self.check_for_row_in_list_table('1: Buy milk')
         # Perversely do same again and receives similar warning on list page.
-
+        self.browser.find_element_by_id('id_new_item').send_keys('\n')
         # And she can correct by filling some test in
+        self.check_for_row_in_list_table('1: Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+        self.check_for_row_in_list_table('1: Buy milk')
+        self.check_for_row_in_list_table('2: Make tea')
         self.fail('write me!')
 
 if __name__ == '__main__':
