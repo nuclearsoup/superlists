f25c69ee8f0074a80dd9f3d8fbfa50f517164e13
nuclearsoup <kevinpercy@gmail.com>
2014-06-12 15:12:18 +0100

    created server tools, management commands to work for staging server

--- superlists/settings.py
+++ superlists/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists',
     'accounts',
+    'lists',
+    'functional_tests',
 )
 
 MIDDLEWARE_CLASSES = (
